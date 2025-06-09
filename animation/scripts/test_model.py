2ea4b8b3103ef2d9306e784a4f837da7c5bcd96f
kzkwoka <kinga.kwoka.3@gmail.com>
2025-06-09 09:05:05 +0200

    Add optical flow to model testing

--- animation/scripts/test_model.py
+++ animation/scripts/test_model.py
@@ -3,24 +3,25 @@ from torch.utils.data import DataLoader
 import lightning as L
 from lightning.pytorch.loggers import WandbLogger
 
-from src.dataset import AnimationTripletDataset
-from src.model import AnimationUNet
+from animation.src.dataset import AnimationTripletDataset
+from animation.src.model import AnimationUNet
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--test_dir", type=str, required=True, help="Path to test dataset")
     parser.add_argument("--ckpt_path", type=str, required=True, help="Path to model checkpoint (.ckpt)")
-    parser.add_argument("--batch_size", type=int, default=8, help="Batch size for test loader")
+    parser.add_argument("--batch_size", type=int, default=128, help="Batch size for test loader")
     parser.add_argument("--channels", type=int, default=3, help="Number of image channels (usually 3)")
+    parser.add_argument("--optical_flow", action="store_true", help="Use optical flow as input")
     parser.add_argument("--project", type=str, default="animation-interpolation", help="WandB project name")
-    parser.add_argument("--run_name", type=str, default="test_unet", help="WandB run name")
+    parser.add_argument("--run_name", type=str, default="test_unet_128_last", help="WandB run name")
     return parser.parse_args()
 
 
 def main(args):
-    test_dataset = AnimationTripletDataset(args.test_dir)
-    test_loader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, num_workers=4)
+    test_dataset = AnimationTripletDataset(args.test_dir, args.optical_flow)
+    test_loader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, num_workers=8)
 
     model = AnimationUNet.load_from_checkpoint(checkpoint_path=args.ckpt_path, channels=args.channels)
 
